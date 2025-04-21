76638a47098e25a98c1eedef13c94689bc89a9e2
ClarkWilliamsIE <clark@inertiaed.org>
2025-04-21 20:21:34 +1200

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Header    from '../components/header';
 import Hero      from '../components/hero';
 import About     from '../components/about';
 import Makertruck from '../components/makertruck';
+import Support   from '../components/support'
+import Contact   from '../components/contact'
 
 export default function Page() {
   return (
@@ -11,6 +13,8 @@ export default function Page() {
         <Hero />
         <About />
        <Makertruck />
+       <Support />
+       <Contact />
         {/* next sections go here */}
       </main>
     </>
