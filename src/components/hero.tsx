d285c1bde044c6f4386cd239cfd12fcdff9ad4cf
ClarkWilliamsIE <clark@inertiaed.org>
2025-04-22 15:03:52 +1200

    heropics

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,12 +4,12 @@
 import React, { useState, useEffect } from 'react';
 
 const images = [
-  '/images/hero1.jpg',
-  '/images/hero2.jpg',
-  '/images/hero3.jpg',
-  '/images/hero4.jpg',
-  '/images/hero5.jpg',
-  '/images/hero6.jpg
+  '/images/c1.jpg',
+  '/images/truck2.png',
+  '/images/c9.jpg',
+  '/images/c4.jpg',
+  '/images/c5.jpg',
+  '/images/c6.jpg
 ];
 
 export default function Hero() {
